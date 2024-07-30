ac9d6efaeea635b15e9caf092eeb9387c9da0e94
Carrotzpc <Carrotzpc@users.noreply.github.com>
2024-07-30 03:36:46 +0000

    deploy: 07b1c0e99c6cc447e042451aa7301d00be5130ab

---
+++ meta__Example.cb573612.async.js
@@ -0,0 +1,408 @@
+(self.webpackChunkdumi_theme_yunti_example=self.webpackChunkdumi_theme_yunti_example||[]).push([[9554],{56142:function(d,t,n){var c={"./ar.json":[42629,2629],"./be.json":[97130,7130],"./cs.json":[19151,9151],"./de.json":[1490,1490],"./en.json":[52729,2729],"./es.json":[74738,4738],"./fa.json":[74117,4117],"./fi.json":[77722,7722],"./fr.json":[21633,1633],"./hi.json":[34125,4125],"./it.json":[46612,6612],"./ja.json":[36042,6042],"./ko.json":[4019,4019],"./nl.json":[34094,4094],"./pl.json":[38127,8127],"./pt.json":[55982,5982],"./ru.json":[88616,8616],"./sa.json":[58260,8260],"./tr.json":[42772,2772],"./uk.json":[98349,8349],"./vi.json":[47627,7627],"./zh.json":[18061,8061]};function o(p){if(!n.o(c,p))return Promise.resolve().then(function(){var g=new Error("Cannot find module '"+p+"'");throw g.code="MODULE_NOT_FOUND",g});var m=c[p],r=m[0];return n.e(m[1]).then(function(){return n.t(r,19)})}o.keys=function(){return Object.keys(c)},o.id=56142,d.exports=o},87802:function(d,t,n){"use strict";n.r(t),n.d(t,{demos:function(){return h}});var c=n(29008),o=n.n(c),p=n(70958),m=n.n(p),r=n(92379),g=n(24780),h={"src-example-demo-demos":{component:r.memo(r.lazy(function(){return Promise.all([n.e(1051),n.e(22),n.e(3204),n.e(2433)]).then(n.bind(n,65063))})),asset:{type:"BLOCK",id:"src-example-demo-demos",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(25639).Z},"@lobehub/ui":{type:"NPM",value:"1.146.9"},leva:{type:"NPM",value:"0.9.35"},"lucide-react":{type:"NPM",value:"0.414.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@lobehub/ui":n(41337),leva:n(75521),"lucide-react":n(37105),react:n(92379)},renderOpts:{compile:function(){var s=m()(o()().mark(function i(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(9134).then(n.bind(n,59134));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},i)}));function l(){return s.apply(this,arguments)}return l}()}},"src-example-demo-breadcrumb":{component:r.memo(r.lazy(function(){return Promise.all([n.e(1051),n.e(22),n.e(3204),n.e(2433)]).then(n.bind(n,92054))})),asset:{type:"BLOCK",id:"src-example-demo-breadcrumb",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(2530).Z},"@lobehub/ui":{type:"NPM",value:"1.146.9"},antd:{type:"NPM",value:"5.19.3"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@lobehub/ui":n(41337),antd:n(28793),react:n(92379)},renderOpts:{compile:function(){var s=m()(o()().mark(function i(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(9134).then(n.bind(n,59134));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},i)}));function l(){return s.apply(this,arguments)}return l}()}},"src-example-demo-procard":{component:r.memo(r.lazy(function(){return Promise.all([n.e(1051),n.e(22),n.e(3204),n.e(2433)]).then(n.bind(n,46789))})),asset:{type:"BLOCK",id:"src-example-demo-procard",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(82991).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.54"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(16231),react:n(92379)},renderOpts:{compile:function(){var s=m()(o()().mark(function i(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(9134).then(n.bind(n,59134));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},i)}));function l(){return s.apply(this,arguments)}return l}()}},"src-example-demo-table":{component:r.memo(r.lazy(function(){return Promise.all([n.e(1051),n.e(22),n.e(3204),n.e(2433)]).then(n.bind(n,95510))})),asset:{type:"BLOCK",id:"src-example-demo-table",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(49353).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.54"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(16231),react:n(92379)},renderOpts:{compile:function(){var s=m()(o()().mark(function i(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(9134).then(n.bind(n,59134));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},i)}));function l(){return s.apply(this,arguments)}return l}()}},"src-example-demo-form":{component:r.memo(r.lazy(function(){return Promise.all([n.e(1051),n.e(22),n.e(3204),n.e(2433)]).then(n.bind(n,47730))})),asset:{type:"BLOCK",id:"src-example-demo-form",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(73894).Z},"@yuntijs/ui":{type:"NPM",value:"1.0.0-beta.54"},"lucide-react":{type:"NPM",value:"0.414.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@yuntijs/ui":n(16231),"lucide-react":n(37105),react:n(92379)},renderOpts:{compile:function(){var s=m()(o()().mark(function i(){var a,u=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(9134).then(n.bind(n,59134));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},i)}));function l(){return s.apply(this,arguments)}return l}()}}}},40456:function(d,t,n){"use strict";n.r(t),n.d(t,{texts:function(){return o}});var c=n(24780);const o=[]},2530:function(d,t){"use strict";t.Z=`import { StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import type { BreadcrumbProps } from 'antd';
+import { Breadcrumb } from 'antd';
+import React from 'react';
+
+const items = [
+  {
+    path: '/apps',
+    title: 'Application List',
+  },
+  {
+    path: '/detail/app-1',
+    title: 'Detail',
+  },
+  {
+    path: '/versions',
+    title: 'Version',
+  },
+];
+
+export default () => {
+  const store = useCreateStore();
+  const control: BreadcrumbProps | any = useControls(
+    {
+      separator: '/',
+    },
+    { store }
+  );
+  return (
+    <StoryBook levaStore={store}>
+      <Breadcrumb {...control} items={items} />
+    </StoryBook>
+  );
+};
+`},73894:function(d,t){"use strict";t.Z=`import { Divider, Flex, Form, Highlighter, Input } from '@yuntijs/ui';
+import { VariableIcon } from 'lucide-react';
+import React, { useState } from 'react';
+
+const FormCollapseList = Form.CollapseList;
+interface FieldType {
+  outputs?: {
+    key: string;
+    value: string;
+  }[];
+}
+
+export default () => {
+  const outputs = [
+    {
+      key: 'class',
+      value: 'claas1',
+    },
+    {
+      key: 'namespace',
+      value: 'ns1',
+    },
+  ];
+
+  const [values, setValues] = useState<FieldType>({ outputs });
+
+  return (
+    <Flex gap={6} style={{ width: '100%' }} vertical>
+      <Form onValuesChange={(_, values) => setValues(values)} variant="filled">
+        <FormCollapseList
+          columns={[
+            {
+              dependencies: field => [['outputs', field.name, 'value']],
+              label: '\u53D8\u91CF\u540D',
+              name: 'key',
+              render: () => <Input maxLength={30} placeholder="\u8F93\u5165\u53D8\u91CF\u540D" showCount />,
+              rules: (_field, index) => [
+                { whitespace: true, required: true, message: '\u8BF7\u8F93\u5165\u53D8\u91CF\u540D' },
+                ({ getFieldValue }) => ({
+                  validator(_rule, value) {
+                    const vars: FieldType['outputs'] = getFieldValue('outputs');
+                    if (vars?.some((v, vIndex) => vIndex !== index && v.key === value)) {
+                      return Promise.reject(new Error('\u53D8\u91CF\u540D\u4E0D\u80FD\u91CD\u590D'));
+                    }
+                    return Promise.resolve();
+                  },
+                }),
+              ],
+            },
+            {
+              label: '\u53D8\u91CF\u503C',
+              name: 'value',
+              render: () => <Input placeholder="\u8F93\u5165\u53D8\u91CF\u503C" style={{ width: 360 }} />,
+              rules: [{ required: true, message: '\u8BF7\u8F93\u5165\u53D8\u91CF\u503C' }],
+            },
+          ]}
+          icon={VariableIcon}
+          initialValue={outputs}
+          name="outputs"
+          title="\u8F93\u51FA\u53D8\u91CF"
+          variant="block"
+        />
+      </Form>
+      <Divider
+        content="Form value \u503C"
+        dashed
+        defaultOpen
+        mode="expanded"
+        orientation="left"
+        orientationMargin={0}
+      >
+        <Highlighter language="json">{JSON.stringify(values, null, 2)}</Highlighter>
+      </Divider>
+    </Flex>
+  );
+};
+`},82991:function(d,t){"use strict";t.Z=`import { Flex, ProCard, Tag, Typography } from '@yuntijs/ui';
+import React from 'react';
+
+const { Time } = Typography;
+
+const ProCardBorderedDemo = () => {
+  return (
+    <ProCard bordered hoverable={false} style={{ width: 420 }}>
+      <ProCard.Header
+        description="\u6211\u662F\u63CF\u8FF0"
+        icon={{
+          src: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        }}
+        title="\u6211\u662F\u6807\u9898"
+      />
+      <ProCard.Content>
+        <ProCard.Descriptions
+          column={2}
+          items={[
+            {
+              key: 'creator',
+              label: '\u521B\u5EFA\u8005',
+              children: 'Luobo Zhang',
+            },
+            {
+              key: 'tags',
+              contentStyle: {
+                width: '100%',
+              },
+              children: (
+                <Flex gap={4} justify="flex-end">
+                  <Tag color="geekblue">LLM</Tag>
+                  <Tag color="green">Embedding</Tag>
+                </Flex>
+              ),
+            },
+            {
+              key: 'updateTime',
+              label: '\u66F4\u65B0\u65F6\u95F4',
+              children: <Time relativeTime={false} time={new Date().toISOString()} />,
+            },
+          ]}
+        />
+      </ProCard.Content>
+    </ProCard>
+  );
+};
+
+export default ProCardBorderedDemo;
+`},49353:function(d,t){"use strict";t.Z=`import { Dropdown, Table, TableProps, Tag } from '@yuntijs/ui';
+import React, { useState } from 'react';
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  tags: string[];
+  description: string;
+}
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'The YuntiUI components are inspired by LobeUI and developed based on Antd components, fully compatible with Antd components, and it is recommended to use antd-style as the default css-in-js styling solution.',
+    age: 32,
+    tags: ['nice', 'developer'],
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    tags: ['loser'],
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: '3',
+    name: 'Not Expandable',
+    age: 29,
+    description: 'This not expandable',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '4',
+    name: 'Joe Black',
+    age: 32,
+    description: 'My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+type OnChange = NonNullable<TableProps<DataType>['onChange']>;
+type Filters = Parameters<OnChange>[1];
+
+type GetSingle<T> = T extends (infer U)[] ? U : never;
+type Sorts = GetSingle<Parameters<OnChange>[2]>;
+
+export default () => {
+  const [current, setCurrent] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [total] = useState<number>(data.length);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [loading] = useState(false);
+
+  const [filters, setFilters] = useState<Filters>({});
+  const [soter, setSorter] = useState<Sorts>({});
+
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      ellipsis: true,
+      render: text => text,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      sorter: (a, b) => a.age - b.age,
+      sortOrder: soter.columnKey === 'age' ? soter.order : null,
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      filters: [
+        { text: 'nice', value: 'nice' },
+        { text: 'cool', value: 'cool' },
+      ],
+      filteredValue: filters.tags || null,
+      onFilter: (value, record) => record.tags.includes(value as string),
+      width: 200,
+      render: (_, { tags }) => (
+        <>
+          {tags.map(tag => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: 130,
+      render: (_, record) => (
+        <Dropdown.Button
+          menu={{
+            items: [
+              {
+                key: 'detail',
+                label: '\u8BE6\u60C5',
+              },
+            ],
+            onClick: ({ key }) => {
+              console.warn('click', key, record);
+            },
+          }}
+          onClick={() => {
+            console.warn('\u5220\u9664', record);
+          }}
+          overlayStyle={{
+            width: 100,
+          }}
+        >
+          \u5220\u9664
+        </Dropdown.Button>
+      ),
+    },
+  ];
+
+  const handlePaginationChange = (newCurrent: number, newPageSize: number) => {
+    setCurrent(newCurrent);
+    setPageSize(newPageSize);
+  };
+  const handleTableChange: TableProps<DataType>['onChange'] = (
+    pagination,
+    newFilters,
+    newSoter
+  ) => {
+    setCurrent(pagination.current || 0);
+    setPageSize(pagination.pageSize || 0);
+    setFilters(newFilters);
+    setSorter(newSoter as Sorts);
+  };
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      expandable={{
+        expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+        rowExpandable: record => record.name !== 'Not Expandable',
+      }}
+      loading={loading}
+      onChange={handleTableChange}
+      pagination={{
+        current,
+        onChange: handlePaginationChange,
+        onShowSizeChange: handlePaginationChange,
+        pageSize,
+        showTotal: newTotal => \`\u5171\u8BA1 \${newTotal} \u6761\`,
+        total,
+      }}
+      rowSelection={{
+        selectedRowKeys,
+        onChange: (newSelectedRowKeys: React.Key[]) => {
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      }}
+    />
+  );
+};
+`},25639:function(d,t){"use strict";t.Z=`import { ActionIcon, ActionIconProps, StoryBook, useControls, useCreateStore } from '@lobehub/ui';
+import { folder } from 'leva';
+import { Settings } from 'lucide-react';
+import React from 'react';
+
+export default () => {
+  const store = useCreateStore();
+  const size: ActionIconProps['size'] | any = useControls(
+    {
+      size: folder({
+        blockSize: {
+          max: 100,
+          min: 8,
+          step: 4,
+          value: 40,
+        },
+        borderRadius: {
+          max: 50,
+          min: 2,
+          step: 2,
+          value: 10,
+        },
+        fontSize: {
+          max: 100,
+          min: 8,
+          step: 4,
+          value: 28,
+        },
+        strokeWidth: {
+          max: 2,
+          min: 1,
+          step: 0.5,
+          value: 2,
+        },
+      }),
+    },
+    { store }
+  );
+
+  const control: ActionIconProps | any = useControls(
+    {
+      active: false,
+      glass: false,
+      tooltip: folder({
+        arrow: false,
+        loading: false,
+        placement: {
+          options: [
+            'top',
+            'left',
+            'right',
+            'bottom',
+            'topLeft',
+            'topRight',
+            'bottomLeft',
+            'bottomRight',
+            'leftTop',
+            'leftBottom',
+            'rightTop',
+            'rightBottom',
+          ],
+          value: 'top',
+        },
+        spotlight: false,
+        title: 'Setting',
+      }),
+    },
+    { store }
+  );
+
+  return (
+    <StoryBook levaStore={store}>
+      <ActionIcon icon={Settings} size={size} {...control} />
+    </StoryBook>
+  );
+};
+`}}]);
