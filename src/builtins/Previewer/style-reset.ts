03be61648557c29fe7fcc65bb32d9d9b96eb6a13
Carrotzpc <zhang.pc3@gmail.com>
2024-07-30 11:31:09 +0800

    fix(Previewer): change to revert style

--- src/builtins/Previewer/style-reset.ts
+++ src/builtins/Previewer/style-reset.ts
@@ -1,58 +1,286 @@
 import { css } from 'antd-style';
 
+// reset style of https://github.com/lobehub/lobe-ui/blob/master/src/Markdown/markdown.style.ts
 export const resetStyle = css`
-  blockquote,
-  code,
+  /* stylelint-disable */
+  a {
+    color: revert;
+
+    &:hover {
+      color: revert;
+    }
+  }
+
+  blockquote {
+    margin-block: revert;
+    margin-inline: revert;
+    padding-block: revert;
+    padding-inline: revert;
+
+    color: revert;
+
+    border-inline-start: revert;
+  }
+  code {
+    display: revert;
+
+    margin-inline: revert;
+    padding-block: revert;
+    padding-inline: revert;
+
+    font-family: revert;
+    font-size: revert;
+    line-height: revert;
+    word-break: revert;
+    white-space: revert;
+
+    background: revert;
+    border: revert;
+    border-radius: revert;
+  }
+
+  details {
+    margin-block: revert;
+    padding-block: revert;
+    padding-inline: revert;
+
+    background: revert;
+    border-radius: revert;
+    box-shadow: revert;
+
+    summary {
+      cursor: revert;
+      display: revert;
+      align-items: revert;
+      list-style: revert;
+
+      &::before {
+        content: revert;
+
+        position: revert;
+        inset-inline-end: revert;
+        transform: revert;
+
+        display: revert;
+
+        width: revert;
+        height: revert;
+
+        font-family: revert;
+
+        border-block-end: revert;
+        border-inline-end: revert;
+
+        transition: revert;
+      }
+
+      &[open] {
+        summary {
+          padding-block-end: revert;
+          border-block-end: revert;
+
+          &::before {
+            transform: revert;
+          }
+        }
+      }
+    }
+  }
+
   h1,
   h2,
   h3,
   h4,
   h5,
-  h6,
-  hr,
-  img,
-  kbd,
-  p,
-  pre,
-  strong,
-  video {
-    all: unset;
+  h6 {
+    margin-block: revert;
+    font-weight: revert;
+    line-height: revert;
   }
 
-  details {
-    all: unset;
+  h1 {
+    font-size: revert;
+  }
 
-    summary {
-      all: unset;
+  h2 {
+    font-size: revert;
+  }
+
+  h3 {
+    font-size: revert;
+  }
+
+  h4 {
+    font-size: revert;
+  }
+
+  h5,
+  h6 {
+    font-size: revert;
+  }
+
+  hr {
+    margin-block: revert;
+
+    border-color: revert;
+    border-style: revert;
+    border-width: revert;
+    border-block-start: revert;
+    border-inline-start: revert;
+    border-inline-end: revert;
+  }
+
+  img {
+    max-width: revert;
+  }
+
+  > img,
+  > p > img {
+    margin-block: revert;
+    border-radius: revert;
+    box-shadow: revert;
+  }
+
+  kbd {
+    cursor: revert;
+
+    display: revert;
+
+    min-width: revert;
+    margin-inline: revert;
+    padding-block: revert;
+    padding-inline: revert;
+
+    font-family: revert;
+    font-size: revert;
+    font-weight: revert;
+    line-height: revert;
+    text-align: revert;
+
+    background: revert;
+    border: revert;
+    border-radius: revert;
+  }
+
+  li {
+    margin-block: revert;
+
+    p {
+      display: revert;
     }
   }
 
-  a {
-    color: unset;
+  ul,
+  ol {
+    margin-block: revert;
+    margin-inline-start: revert;
+    padding-inline-start: revert;
+    list-style-position: revert;
 
-    &:hover {
-      color: unset;
+    ul,
+    ol {
+      margin-block: revert;
+    }
+
+    li {
+      margin-inline-start: revert;
     }
   }
 
-  ol,
+  ol {
+    list-style: revert;
+  }
+
   ul {
-    all: unset;
+    list-style-type: revert;
 
-    & > li {
-      all: unset;
+    li {
+      &::before {
+        content: revert;
+        display: revert;
+        margin-inline: revert;
+        opacity: revert;
+      }
     }
+  }
+
+  p {
+    margin-block: revert;
+    line-height: revert;
+    letter-spacing: revert;
+  }
+
+  pre,
+  [data-code-type='highlighter'] {
+    white-space: revert;
+    border: revert;
+
+    > code {
+      padding: revert;
+
+      font-family: revert;
+      font-size: revert;
+      line-height: revert;
+
+      border: revert;
+    }
+  }
+
+  strong {
+    font-weight: revert;
   }
 
   table {
-    all: unset;
+    unicode-bidi: revert;
+    overflow: revert;
+    display: revert;
+    border-spacing: revert;
+    border-collapse: revert;
 
-    code,
-    thead,
-    tr,
+    box-sizing: revert;
+    width: revert;
+    max-width: revert;
+    margin-block: revert;
+
+    text-align: revert;
+    text-indent: revert;
+    text-wrap: revert;
+    word-break: revert;
+    overflow-wrap: revert;
+
+    background: revert;
+    border-radius: revert;
+    box-shadow: revert;
+
+    code {
+      word-break: revert;
+    }
+
+    thead {
+      background: revert;
+    }
+
+    tr {
+      box-shadow: revert;
+    }
+
     th,
     td {
-      all: unset;
+      min-width: revert;
+      padding-block: revert;
+      padding-inline: revert;
+      text-align: initial;
     }
+  }
+
+  > video,
+  > p > video {
+    margin-block: revert;
+    border-radius: revert;
+    box-shadow: revert;
+  }
+
+  video {
+    max-width: revert;
   }
 `;
